9f9f38903a72bcc67c735f5a71b5b2d15efa205a
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-07-15 17:49:43 +0530

    url routes

--- src/modules/urls/url.routes.js
+++ src/modules/urls/url.routes.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { createUrl, getMyUrls } from './url.controller.js';
 import { verifyJWT } from '../auth/auth.middleware.js';
+import { verifyApiKey } from '../apikeys/apiKey.middleware.js';
+import { rateLimiter } from '../rateLimiter/rateLimiter.middleware.js';
 
 const router = express.Router();
 
 router.post('/', verifyJWT, createUrl);
 router.get('/', verifyJWT, getMyUrls);
+
+router.post('/via-key' , verifyApiKey , rateLimiter , createUrl)
 
 export default router;
