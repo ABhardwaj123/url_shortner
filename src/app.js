418d4b9e9b230c44b9eadc9a511442d580cc6eb3
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-07-17 17:10:23 +0530

    app instance with routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import authRoutes from './modules/auth/auth.routes.js';
+import urlRoutes from './modules/urls/url.routes.js'
+import redirectRoutes from './modules/urls/redirect.routes.js'
+import analyticsRoutes from './modules/analytics/analytics.routes.js';
 
 const app = express();
 
@@ -8,5 +11,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/urls' , urlRoutes)
+app.use('/', redirectRoutes)
+
+app.use('/api/analytics', analyticsRoutes);
 
 export default app;
